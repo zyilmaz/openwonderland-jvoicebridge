d82031e3957c00e34bcff4e23456b656b24760e4
jprovino <jprovino@88245f68-24f4-11df-a141-03b152defb57>
2008-03-13 17:11:55 +0000

    Update README.  Copy powerpc and intel native libraries.

--- trunk/softphone/audioframework/coreaudio/build.sh
+++ trunk/softphone/audioframework/coreaudio/build.sh
@@ -4,3 +4,5 @@ echo "Complie C++..."
 g++ -c -g -I/System/Library/Frameworks/JavaVM.framework/Headers/ -I/System/Library/Frameworks/CoreAudio.framework/Headers/ AudioDriverMac.cpp
 echo "Link C++..."
 g++ -g -dynamiclib -o libMediaFramework.jnilib AudioDriverMac.o -framework JavaVM -framework CoreAudio -framework AudioUnit -framework AudioToolBox -framework Carbon
+cp libMediaFramework.jnilib ../../build/lib/intel.libMediaFramework.jnilib
+cp libMediaFramework.jnilib ../../build/lib/powerpc.libMediaFramework.jnilib
