7fd8cae3ec8c802138f528488b4d6ccc5d90b9d6
jprovino <jprovino@88245f68-24f4-11df-a141-03b152defb57>
2008-06-25 13:42:40 +0000

    Added -mmacosx-version-min=10.4

--- trunk/softphone/audioframework/coreaudio/build.sh
+++ trunk/softphone/audioframework/coreaudio/build.sh
@@ -1,7 +1,7 @@
 echo "Make Header..."
 javah -classpath ../../build/lib/softphone.jar com.sun.mc.softphone.media.coreaudio.AudioDriverMac
 echo "Complie C++..."
-g++ -arch x86_64 -arch i386 -arch ppc -c -g -I/System/Library/Frameworks/JavaVM.framework/Headers/ -I/System/Library/Frameworks/CoreAudio.framework/Headers/ AudioDriverMac.cpp
+g++ -arch x86_64 -arch i386 -arch ppc -mmacosx-version-min=10.4 -c -g -I/System/Library/Frameworks/JavaVM.framework/Headers/ -I/System/Library/Frameworks/CoreAudio.framework/Headers/ AudioDriverMac.cpp
 echo "Link C++..."
-g++ -arch x86_64 -arch i386 -arch ppc -g -dynamiclib -o libMediaFramework.jnilib AudioDriverMac.o -framework JavaVM -framework CoreAudio -framework AudioUnit -framework AudioToolBox -framework Carbon
+g++ -arch x86_64 -arch i386 -arch ppc -mmacosx-version-min=10.4 -g -dynamiclib -o libMediaFramework.jnilib AudioDriverMac.o -framework JavaVM -framework CoreAudio -framework AudioUnit -framework AudioToolBox -framework Carbon
 cp libMediaFramework.jnilib ../../build/lib
