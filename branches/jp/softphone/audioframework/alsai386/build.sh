079bc6df8a130356a829909025eb79f0c35d2337
jprovino <jprovino@88245f68-24f4-11df-a141-03b152defb57>
2009-08-19 19:40:20 +0000

    Rename alsa native libraries for each architecture.

--- branches/jp/softphone/audioframework/alsai386/build.sh
+++ branches/jp/softphone/audioframework/alsai386/build.sh
@@ -3,4 +3,4 @@ javah -classpath ../../build/lib/softphone.jar com.sun.mc.softphone.media.alsa.A
 echo "Compile..."
 gcc -c -O AudioDriverAlsa.c Setup.c GetAudioDevices.c
 echo "Link..."
-gcc -shared -W1,-soname,libMediaFramework32.so -o libMediaFramework32.so AudioDriverAlsa.o Setup.o GetAudioDevices.o -lasound
+gcc -shared -W1,-soname,libMediaFrameworkI386.so -o libMediaFrameworkI386.so AudioDriverAlsa.o Setup.o GetAudioDevices.o -lasound
