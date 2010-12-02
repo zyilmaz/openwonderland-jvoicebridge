e9fcc526004fba6f29442534150341e9abc1566a
jonathankap <jonathankap@88245f68-24f4-11df-a141-03b152defb57>
2010-12-02 17:55:59 +0000

    Improved audio support on Linux  - use ALSA "default" device, which should point to PulseAudio  - better handling for buffer underruns

--- softphone/audioframework/alsai386/build.sh
+++ softphone/audioframework/alsai386/build.sh
@@ -1,6 +1,10 @@
+JAVA_INCLUDE_DIRS="-I/usr/lib/jvm/java-6-sun/include -I/usr/lib/jvm/java-6-sun/include/linux"
+
+rm *.o *.so
+
 echo "Make Header..."
 javah -classpath ../../build/lib/softphone.jar com.sun.mc.softphone.media.alsa.AudioDriverAlsa
 echo "Compile..."
-gcc -c -O AudioDriverAlsa.c Setup.c GetAudioDevices.c
+gcc -c -fPIC -O AudioDriverAlsa.c Setup.c GetAudioDevices.c $JAVA_INCLUDE_DIRS
 echo "Link..."
 gcc -shared -W1,-soname,libMediaFrameworkI386.so -o libMediaFrameworkI386.so AudioDriverAlsa.o Setup.o GetAudioDevices.o -lasound
