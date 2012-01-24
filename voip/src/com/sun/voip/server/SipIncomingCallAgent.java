68359c73aeb0b90e2aa4aaff9c87d2912f56d294
jonathankap <jonathankap@88245f68-24f4-11df-a141-03b152defb57>
2012-01-24 19:09:49 +0000

    For incoming calls, make sure to send the public IP address in the SDP

--- voip/src/com/sun/voip/server/SipIncomingCallAgent.java
+++ voip/src/com/sun/voip/server/SipIncomingCallAgent.java
@@ -1,3 +1,21 @@
+/**
+ * Open Wonderland
+ *
+ * Copyright (c) 2011 - 2012, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition. 
+ * 
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ * 
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
 /*
  * Copyright 2007 Sun Microsystems, Inc.
  *
@@ -157,8 +175,8 @@ public class SipIncomingCallAgent extends CallSetupAgent implements SipListener 
 
             sipServerCallback.addSipListener(sipCallId, this);
 
-    	    InetSocketAddress isa = callHandler.getReceiveAddress();
-
+    	    InetSocketAddress isa = new InetSocketAddress(Bridge.getPublicHost(), 
+                                  callHandler.getReceiveAddress().getPort());
 	    if (isa == null) {
 		Logger.println(
 		    "SipIncomingCallAgent:  can't get receiver socket!");
